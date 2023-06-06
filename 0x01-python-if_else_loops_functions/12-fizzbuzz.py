ee2995034b7a6a7804b6d9aba887b9d93ad8ea47
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-06-06 12:42:06 -0700

    Fix 12-fizzbuzz.py: space missing

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 100+1):
-        if i != 1:
-            print(" ", end="")
         if i % 3 == 0:
-            print("Fizz", end="")
+            print("Fizz", end=" ")
         if i % 5 == 0:
-            print("Buzz", end="")
+            print("Buzz", end=" ")
         if i % 3 != 0 and i % 5 != 0:
-            print(i, end="")
+            print(i, end=" ")
