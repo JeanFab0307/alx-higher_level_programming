cd277545cc006b63a43af20ea2a60984b4411437
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-06-08 13:25:47 -0700

    Fix 2-args.py: case for no input

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,8 @@
 import sys
 
 if __name__ == "__main__":
-    end_mess = "s:" if len(sys.argv) != 2 else ":"
-    print("{} argument{}".format(len(sys.argv) - 1, end_mess))
+    end_mess = "s" if len(sys.argv) != 2 else ""
+    ponct = ":" if len(sys.argv) != 1 else "."
+    print("{} argument{}{}".format(len(sys.argv) - 1, end_mess, ponct))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
