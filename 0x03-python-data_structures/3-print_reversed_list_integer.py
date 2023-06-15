5578b449b88c1454299a8ec3b60f4c1d9372d304
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-06-15 07:39:19 -0700

    Fix 3-print_reversed_list_integer.py: None case added

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
+    if my_list is not None:
+        my_list.reverse()
     for i in my_list:
         number = "{:d}"
         print(number.format(i))
