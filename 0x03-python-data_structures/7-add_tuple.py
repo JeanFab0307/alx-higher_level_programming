897fc37cd9b227ac7939d1ff2a63b8d42373fc8f
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-06-16 05:11:24 -0700

    Fix 7-add_tuple.py: more than 2 elements in tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,8 +3,12 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     result = [0, 0]
     for i in range(len(tuple_a)):
+        if i == 2:
+            break
         result[i] = tuple_a[i]
     for i in range(len(tuple_b)):
+        if i == 2:
+            break
         result[i] += tuple_b[i]
     result = tuple(result)
     return result
