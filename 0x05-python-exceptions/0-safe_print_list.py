4068c03096a748b389937ae9fd0b08ef523494a1
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-06-26 09:12:08 -0700

    Fix case for x = 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,5 +7,6 @@ def safe_print_list(my_list=[], x=0):
         except(TypeError, IndexError):
             i -= 1
             break
-    print("")
+    if i + 1 != 0:
+        print("")
     return i + 1
