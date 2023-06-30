91fed68df8cfd9043a7c8b87525c14626d306992
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-06-30 02:56:08 -0700

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module documentation
+"""
+
+class Square:
+    '''Create a square with a given size
+
+    Text
+
+    Args:
+    __size: the size of the square
+
+    '''
+
+    def __init__(self, __size):
+        self.__size = __size
