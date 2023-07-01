d65644a3f4c07eebf22cab27ba4d7b954bebcf12
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-07-01 07:07:50 -0700

    Add 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This module is dedicated to the creation of a Square class"""
+
+
+class Square:
+    """Create a square with a given size
+
+    Text
+
+    Args:
+    __size(int): the size of the square
+
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """int: the size of the Squre"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Set the value of the attribute size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Public instance method to calculate the area of a Square
+
+        Return:
+            int: The area of the square
+        """
+        return self.__size ** 2
