b79aa7ec0a6570c017a63eac319b76fdc8b159fc
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-07-03 08:04:20 -0700

    Add 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module is reserved for the creation of the class Rectangle.
+It contains everything we need about a rectangle object.
+"""
+
+
+class Rectangle:
+    """This class is about a rectangle, a four-side shape"""
+    pass
