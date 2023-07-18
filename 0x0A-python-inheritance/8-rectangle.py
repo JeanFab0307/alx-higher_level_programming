050bf3577c514412ac2cf20ddf5728c12f84ef8a
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-07-18 00:19:48 -0700

    Correct syntax

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,42 +1,20 @@
 #!/usr/bin/python3
 """
-You can find the class BaseGeometry and all its methods here.
-The class Rectangle exists and inherits from BaseGeometry
+Contain the class Rectangl that inherits from BaseGeometry
 """
 
 
-class BaseGeometry:
-    """
-    The shape of certain elements
-    """
-
-    def area(self):
-        """
-        Raise an exception
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates value
-    Args:
-        name(str): the name of the shape
-        value(int): to see later
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """
     A subclass of BaseGeometry
-Attributes:
+    Attributes:
     width(int): the width of the Rectangle
     heigth(int): the height of the Rectangle
     """
-
+    
     def __init__(self, width, height):
         super().integer_validator("width", width)
         super().integer_validator("heigth", height)
