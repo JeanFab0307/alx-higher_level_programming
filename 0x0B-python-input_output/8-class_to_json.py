5bb66409265b78b261a06301ec83799b40584bd8
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-08-01 08:38:00 -0700

    Remove import and fix code

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Function class_to_obj(obj)"""
-from json import dumps
 
 
 def class_to_json(obj):
     """Returns the dictionary description with simple data structure
     for JSON serialization of an object
     """
-    return dumps(obj.__dict__)
+    return vars(obj)
