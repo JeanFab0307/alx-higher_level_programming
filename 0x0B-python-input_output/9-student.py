62ebe28777f35530cd47a4c170a8bbbcd34c9d6a
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-08-06 21:32:55 -0700

    Add 9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student():
+    """Defines a student
+Args:
+    first_name(str): The first name
+    last_name(str): The last name
+    age(int): the age
+    """
+
+    
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dict representation of a student"""
+        return vars(self)
