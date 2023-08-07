5068eb5902732618e1430d5c3fbd36a4b1cf454f
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-08-06 21:34:30 -0700

    Apply PEP8 style

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,8 +9,6 @@ Args:
     last_name(str): The last name
     age(int): the age
     """
-
-    
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
