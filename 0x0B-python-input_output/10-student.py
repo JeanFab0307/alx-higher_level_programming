669857aeca79f01051852c89ad9e7e01fbdc81c1
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-08-06 22:20:36 -0700

    Add 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student():
+    """Defines a student
+Args:
+    first_name(str): The first name
+    last_name(str): The last name
+    age(int): the age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """Retrieves a dict representation of a student"""
+        if attr is None or type(attr) is not list:
+            return vars(self)
+        else:
+            result = {}
+            for item in attr:
+                if item == 'age':
+                    result[item] = self.age
+                if item == 'first_name':
+                    result[item] = self.first_name
+                if item == 'last_name':
+                    result[item] = self.last_name
+            return result
