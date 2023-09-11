e3f0e7d2f1383c182a2b6dc195faaf4aadf67696
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-09-11 13:15:33 -0700

    Add 9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+
+function add (a, b) {
+    if (isNaN(a) || isNaN(b)) {
+	return NaN;
+    } else {
+	return a + b;
+    }
+}
