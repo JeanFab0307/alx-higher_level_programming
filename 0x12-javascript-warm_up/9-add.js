c7cee0c7cc835668e0682a14c7954022b1f6eb0b
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-09-11 13:16:11 -0700

    Correct style

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,9 +2,9 @@
 console.log(add(Number(process.argv[2]), Number(process.argv[3])));
 
 function add (a, b) {
-    if (isNaN(a) || isNaN(b)) {
-	return NaN;
-    } else {
-	return a + b;
-    }
+  if (isNaN(a) || isNaN(b)) {
+    return NaN;
+  } else {
+    return a + b;
+  }
 }
