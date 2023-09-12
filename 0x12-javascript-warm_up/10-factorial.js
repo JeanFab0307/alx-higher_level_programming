36aad726ca58dacff8a8beb420014a3f0e569eef
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-09-11 17:27:18 -0700

    Add 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+console.log(factorial(Number(process.argv[2])));
+
+function factorial (a) {
+  if (isNaN(a) || a <= 0) {
+    return 1;
+  } else {
+      return a * factorial(a - 1);
+  }
+}
