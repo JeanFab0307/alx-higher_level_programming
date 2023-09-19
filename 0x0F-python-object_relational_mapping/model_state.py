9b40b0d69edbbbde6aa3bac563a9c70ad1747c7d
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-09-19 08:31:51 +0300

    Apply pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,13 @@
 #!/bin/python3
 """Contain the class definition of a State and an instance Base"""
+
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
+
+
 class State(Base):
     """Class state with an id and a name"""
     __tablename__ = "states"
