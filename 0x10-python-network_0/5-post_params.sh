61919bbf2086af9f3811449719bd14f5d1301a0e
JeanFab0307 <jfrandriatsimivony@gamil.com>
2023-09-28 23:47:49 +0300

    Fix output

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # display response after a POST and setting variables
-curl -d  "email: test@gmail.com" -d "subject: I will always be here for PLD" -X POST -s "$1"
+curl -d  "email"="test@gmail.com" -d "subject"="I will always be here for PLD" -X POST -s "$1"
