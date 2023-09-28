936131a22d7c04a62ae0f3bbe1b8322760c7a08f
JeanFab0307 <jfrandriatsimivony@gamil.com>
2023-09-28 23:14:43 +0300

    Correct output

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept
-curl -X OPTIONS -si "$1" | grep "Allow" | awk '{print $1}'
+curl -X OPTIONS -si "$1" | grep "Allow" |cut -f 2- -d " "
