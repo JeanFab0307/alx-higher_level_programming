28cfc98a39728d04954d25997a9a5c7f6637d20f
JeanFab0307 <jfrandriatsimivony@gamil.com>
2023-09-29 15:24:23 +0300

    Add task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Task 0 ! """
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print(f"\t- content: {html}")
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
