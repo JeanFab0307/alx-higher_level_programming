674356c472f162937799fa485d1a4710f9c369ba
JeanFab0307 <jfrandriatsimivony@gamil.com>
2023-10-02 18:05:40 +0300

    Apply pep8 style

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT OUTER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name "
+                "FROM cities LEFT OUTER JOIN states "
+                "ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
